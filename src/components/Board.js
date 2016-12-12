c112cc9897648f5d923a6aa4b2319189f0af4c87
Andrew Kud <kud@mm.st>
2016-12-12 21:05:46 +0200

    add Board class

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
-class Board extends React.Compoent {
+class Board extends React.Component {
 
-    createRows(n) {
+    /*createRows(n) {
         return [...Array(n).keys()].map((i) => {
             <div className="board-row"></div>
         });
-    }
+    }*/
 
     render() {
+        let rows = [...Array(3).keys()].map((i) => {
+                <div className="board-row">foo</div>
+            });
         return (
-            
-
+            <div>{rows}</div>
         );
     }
 }
+
+export default Board;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Board from './Board';
 
 class Game extends React.Component {
     render() {
         return (
             <div className="game-area">
-                ff
+                <Board />
             </div>
         );
     }
