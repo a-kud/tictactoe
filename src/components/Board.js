628782b804e9555e1bd4d14f78487e83bac46b2f
Andrew Kud <kud@mm.st>
2016-12-24 11:38:20 +0200

    implement findWinner

--- src/components/Board.js
+++ src/components/Board.js
@@ -16,6 +16,7 @@ class Board extends React.Component {
                 />
     }
 
+    // n - board index
     handleClick(n) {
         let grid = this.state.grid.slice(),
             xNext = this.state.xNext,
@@ -24,8 +25,37 @@ class Board extends React.Component {
         if (!squareValue) {
             grid[n] = xNext ? "X" : "O";
             this.setState({grid: grid,
-                           xNext: !xNext});
+                           xNext: !xNext}, () => this.findWinner(grid[n]));
+            //console.log("grid[n] ", grid[n])
         }
+    }
+
+    findWinner(player) {
+        // player "X" or "O"
+        // returns array of indicies of winning cells
+
+        const COLUMNS = [
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+        ];
+        const ROWS = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+        ];
+        const DIAGONALS = [
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        let winner = [...COLUMNS, ...ROWS, ...DIAGONALS].filter((v) =>{
+            return v.every((v) => {
+                return this.state.grid[v] === player;
+            });
+        });
+
+        if (winner.length > 0) {console.log(player, "wins"); return winner;}
     }
 
     render() {
